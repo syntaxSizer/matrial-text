e896e51b6a0aaa1ab1f7d981128c6b13630866bb
syntaxSizer <kakuma4never@gmail.com>
2016-05-31 15:01:27 +0300

    text field animated effect ' matrial text effect'

---
+++ app/src/main/java/matrial/aka/matrial_text/Matrial_text.java
@@ -0,0 +1,13 @@
+package matrial.aka.matrial_text;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Matrial_text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matrial_text);
+    }
+}
